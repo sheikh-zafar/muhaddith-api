d95f0729ea16dba025373c67d654ce5b1942fcdd
sheikh-zafar <audios.shkzafar@gmail.com>
2023-05-29 11:52:18 +0400

    commit

--- dars-api.js
+++ dars-api.js
@@ -3,9 +3,9 @@ export const darsApi = [
     id: 1,
     seotitle: 'At targheeb wat tarheeb',
     seodescription: '',
-    audioLink: {
+    audioLink: [{
       link:'http://docs.google.com/uc?export=open&id=1G-E0YcNJuRN8vQsTDyLl9gf4jGdEtt5U',
-      link:'http://docs.google.com/uc?export=open&id=1RU2IpF7zN_j3T7KXlEQUPnCET-WlAg9i',
-    },
+      link:'http://docs.google.com/uc?export=open&id=1RU2IpF7zN_j3T7KXlEQUPnCET-WlAg9i'
+    }],
   },
 ];
