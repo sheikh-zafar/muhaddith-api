dd0fa5910bb9c4b404937b1f20350374103b5094
sheikh-zafar <audios.shkzafar@gmail.com>
2023-05-29 11:56:20 +0400

    commit

--- dars-api.js
+++ dars-api.js
@@ -5,7 +5,10 @@ export const darsApi = [
     seodescription: '',
     audioLink: [{
       link:'http://docs.google.com/uc?export=open&id=1G-E0YcNJuRN8vQsTDyLl9gf4jGdEtt5U',
-      link:'http://docs.google.com/uc?export=open&id=1RU2IpF7zN_j3T7KXlEQUPnCET-WlAg9i'
-    }],
+    },
+	{
+		link:'http://docs.google.com/uc?export=open&id=1RU2IpF7zN_j3T7KXlEQUPnCET-WlAg9i',
+	}
+		],
   },
 ];
