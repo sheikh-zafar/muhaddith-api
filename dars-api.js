740f96e347e03f97319f4590c34792895d9ed2e5
sheikh-zafar <audios.shkzafar@gmail.com>
2023-06-07 11:07:52 +0400

    Commit

--- dars-api.js
+++ dars-api.js
@@ -1,14 +1,40 @@
 export const darsApi = [
   {
     id: 1,
-    seotitle: 'At targheeb wat tarheeb',
-    seodescription: '',
-    audioLink: [{
-      link:'http://docs.google.com/uc?export=open&id=1G-E0YcNJuRN8vQsTDyLl9gf4jGdEtt5U',
-    },
-	{
-		link:'http://docs.google.com/uc?export=open&id=1G-E0YcNJuRN8vQsTDyLl9gf4jGdEtt5U',
-	}
-		],
+    enseotitle: 'At targheeb wat tarheeb',
+    urseotitle: 'At targheeb wat tarheeb',
+    arseotitle: 'At targheeb wat tarheeb',
+    enseodescription: '',
+    urseodescription: '',
+    audioLink: [
+      {
+        link: 'http://docs.google.com/uc?export=open&id=1RU2IpF7zN_j3T7KXlEQUPnCET-WlAg9i',
+      },
+      {
+        link: 'http://docs.google.com/uc?export=open&id=1H4L8wb_0CXlJ_j34smCqcLrto6k41skB',
+      },
+    ],
+  },
+  {
+    id: 2,
+    enseotitle: 'Al-Arbaoona An-Nawawiyyah',
+    urseotitle: 'Al-Arbaoona An-Nawawiyyah',
+    arseotitle: 'Al-Arbaoona An-Nawawiyyah',
+    enseodescription: '',
+    urseodescription: '',
+    audioLink: [
+      {
+        link: 'http://docs.google.com/uc?export=open&id=1HQchWGOCP6DmZDW1SfEOkmIFsAIN6sa8',
+      },
+      {
+        link: 'http://docs.google.com/uc?export=open&id=1xg9vNV50WM26cxmxYTc-1AhncgMZzP9w',
+      },
+      {
+        link: 'http://docs.google.com/uc?export=open&id=11sfZR0c7pu89xuLPh76NrG8Gm2O52BrL',
+      },
+      {
+        link: 'http://docs.google.com/uc?export=open&id=1Bzny-PtjhftTf3Xdc5kohz5Rfsrx51G9',
+      },
+    ],
   },
 ];
