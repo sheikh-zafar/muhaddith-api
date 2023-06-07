7643cd0079f90f1a2020f71774a06dc9339ccb6f
sheikh-zafar <audios.shkzafar@gmail.com>
2023-06-07 11:44:42 +0400

    commit

--- dars-api.js
+++ dars-api.js
@@ -6,13 +6,7 @@ export const darsApi = [
     arseotitle: 'At targheeb wat tarheeb',
     enseodescription: '',
     urseodescription: '',
-    audioLink: [
-      {
-        link: 'http://docs.google.com/uc?export=open&id=1RU2IpF7zN_j3T7KXlEQUPnCET-WlAg9i',
-      },
-      {
-        link: 'http://docs.google.com/uc?export=open&id=1H4L8wb_0CXlJ_j34smCqcLrto6k41skB',
-      },
+    audioLink: ['http://docs.google.com/uc?export=open&id=1RU2IpF7zN_j3T7KXlEQUPnCET-WlAg9i','http://docs.google.com/uc?export=open&id=1H4L8wb_0CXlJ_j34smCqcLrto6k41skB'
     ],
   },
   {
@@ -22,19 +16,7 @@ export const darsApi = [
     arseotitle: 'Al-Arbaoona An-Nawawiyyah',
     enseodescription: '',
     urseodescription: '',
-    audioLink: [
-      {
-        link: 'http://docs.google.com/uc?export=open&id=1HQchWGOCP6DmZDW1SfEOkmIFsAIN6sa8',
-      },
-      {
-        link: 'http://docs.google.com/uc?export=open&id=1xg9vNV50WM26cxmxYTc-1AhncgMZzP9w',
-      },
-      {
-        link: 'http://docs.google.com/uc?export=open&id=11sfZR0c7pu89xuLPh76NrG8Gm2O52BrL',
-      },
-      {
-        link: 'http://docs.google.com/uc?export=open&id=1Bzny-PtjhftTf3Xdc5kohz5Rfsrx51G9',
-      },
+    audioLink: ['http://docs.google.com/uc?export=open&id=1HQchWGOCP6DmZDW1SfEOkmIFsAIN6sa8','http://docs.google.com/uc?export=open&id=1xg9vNV50WM26cxmxYTc-1AhncgMZzP9w','http://docs.google.com/uc?export=open&id=11sfZR0c7pu89xuLPh76NrG8Gm2O52BrL','http://docs.google.com/uc?export=open&id=1Bzny-PtjhftTf3Xdc5kohz5Rfsrx51G9',
     ],
   },
 ];
