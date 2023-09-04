a212fd1ed349ec7edcdcf6bc612353a93b3426fe
sheikh-zafar <audios.shkzafar@gmail.com>
2023-09-04 12:03:33 +0400

    commit

--- pages/api/dars/[name]/[nametwo]/[namethree]/index.js
+++ pages/api/dars/[name]/[nametwo]/[namethree]/index.js
@@ -1,26 +1,13 @@
 import { rootFilesAndFolders } from '../../../../../../dars-api';
 
 export default function handler(req, res) {
-    const { name, nametwo, namethree } = req.query;
-    console.log(nametwo);
-    console.log(name);
-    console.log(namethree);
-    const children = rootFilesAndFolders.filter(obj => {
-        obj.name === name &&
-            obj.children.flatMap(j => j.name === nametwo) &&
-            obj.children.flatMap(p => p.children.map(g => g.name));
-        console.log(
-            obj.name === name &&
-                obj.children.flatMap(j => j.name === nametwo) &&
-                obj.children.flatMap(p => p.children.map(g => g.name))
-        );
-    });
-    const filter = rootFilesAndFolders.filter(o => {
-        if (o.name === name && o.children.map(j => j.name === nametwo)) {
-            o.children.map(i => i.children.map(h => h.name === namethree));
-        }
-    });
-    console.log(filter);
+  const { name, nametwo, namethree } = req.query;
 
-    res.status(200).json(filter);
+  const children = rootFilesAndFolders.flatMap(
+    (obj) =>
+      obj.name === name &&
+      obj.children.filter((j) => j.name === nametwo).map((c) => c.children)
+  );
+
+  res.status(200).json(children);
 }
